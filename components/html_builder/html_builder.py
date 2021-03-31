c5f82988048821272f65af911ff4d260d91cd7d5
uigctaw <uigctaw@protonmail.com>
2021-03-31 23:01:08 +0100

    Under construction warning + lorem ipsum + basic css

--- components/html_builder/html_builder.py
+++ components/html_builder/html_builder.py
@@ -40,11 +40,25 @@ class html(E):
             name = 'viewport'
             content = 'width=device-width, initial-scale=1.0'
 
+        class link:
+            
+            rel = 'stylesheet'
+            href = 'style.css'
+            
+
     class body(E):
 
-        class h1:
+        class strong:
 
-            TEXT = 'Under construction'
+            class h1:
+
+                TEXT = 'Under construction'
+
+        class p:
+
+            TEXT = '''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac fermentum magna, sed tempus justo. Etiam justo sapien, facilisis non viverra hendrerit, consectetur ut felis. Praesent sit amet ultricies erat, sed sagittis nisl. Etiam neque diam, consequat in lacus in, molestie pulvinar nunc. Ut sit amet tellus quis lectus fringilla malesuada. Duis fermentum dictum elit vel eleifend. Sed tincidunt quam at dolor mattis, vel condimentum lorem lacinia. Vivamus rhoncus metus quis sapien euismod, sed pulvinar diam interdum. Nam ligula turpis, posuere eu nibh et, cursus dapibus metus. Fusce ac bibendum turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed tempor magna id urna tincidunt commodo. Vivamus ultricies quam eros, vel consequat sem vestibulum vitae. Sed vestibulum hendrerit tellus, et iaculis ipsum sodales sit amet. 
+            '''
 
 
 def create_file(fs):
@@ -57,6 +71,26 @@ def create_file(fs):
         fh.write(content.encode('utf-8'))
 
 
+def create_css_file(fs):
+    fs.mkdir((STATIC_FILES_FOLDER,), exist_ok=True)
+
+    content = textwrap.dedent('''
+        html {
+            font-family: "Courier New", Courier, monospace;
+            max-width: 64ch;
+            margin: auto;
+        }
+        h1 {
+            text-align: center;
+        }
+        ''')
+    print('Writing:\n')
+    print(content)
+    with fs.open((STATIC_FILES_FOLDER, 'style.css',), 'wb') as fh:
+        fh.write(content.encode('utf-8'))
+
+
 if __name__ == '__main__':
     fs = RealFileSystem()
     create_file(fs)
+    create_css_file(fs)
